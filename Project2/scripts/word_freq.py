91ee622dcfadaa99b52b051f1e8f50f615eccdb4
gj0706 <32847054+gj0706@users.noreply.github.com>
2019-04-01 21:54:24 -0500

    initial commit

---
+++ Project2/scripts/word_freq.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 27 11:49:48 2019
+
+@author: Jian Guo
+"""
+
+import pandas as pd
+
+# read file and create data frame
+data = pd.read_csv('/Users/jaywang/Documents/TTU_study/Spring2019/CS5331DataVisualization/Projects/Project2/data/athletes.csv',header=0)
+df = pd.DataFrame(data, columns=data.columns)
+
+
+# compute top 50 highest frequent words in each category
+name_count = df["Name"].value_counts()[0:50]
+team_count = df["Team"].value_counts()[0:50]
+year_count = df["Year"].value_counts()[0:50]
+city_count = df["City"].value_counts()[0:50]
+sport_count = df["Sport"].value_counts()[0:50]
+event_count = df["Event"].value_counts()[0:50]
+
+# convert above serieses to data frames
+df_name = pd.DataFrame({"name": name_count.index, "n_frequency": name_count.values} )
+df_team = pd.DataFrame({"team": team_count.index, "t_frequency": team_count.values})
+df_year = pd.DataFrame({"year": year_count.index, "y_frequency": year_count.values})
+df_city = pd.DataFrame({"city": city_count.index, "c_frequency": city_count.values})
+df_sport = pd.DataFrame({"sport": sport_count.index, "s_frequency": sport_count.values})
+df_event = pd.DataFrame({"event": event_count.index, "e_frequency": event_count.values})
+
+# conbmine and write to csv file
+df_left = df_name.join(df_team)
+df_right = df_city.join(df_sport) 
+df_freq = df_left.join(df_right).join(df_event)
+df_freq.to_csv("word_freq.csv")
---
+++ Project2/scripts/yearly_freq.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 30 13:57:29 2019
+
+@author: Jian Guo
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# read the data
+data = pd.read_csv('/Users/jaywang/Documents/TTU_study/Spring2019/CS5331DataVisualization/Projects/Project2/data/athletes.csv',header=0)
+data_freq = pd.read_csv('/Users/jaywang/Documents/TTU_study/Spring2019/CS5331DataVisualization/Projects/Project2/data/word_freq.csv',header=0)
+
+# set data frame
+df = pd.DataFrame(data, columns=data.columns)
+df_freq = pd.DataFrame(data_freq, columns=data_freq.columns)
+
+# group data by year-team, year-city and year-sport
+year_name = df.groupby(["Year", "Name"])["Name"].count()
+year_team = df.groupby(["Year", "Team"])["Team"].count()
+year_city = df.groupby(["Year", "City"])["City"].count()
+year_sport = df.groupby(["Year", "Sport"])["Sport"].count()
+year_event = df.groupby(["Year", "Event"])["Event"].count()
+
+
+years = list(set(data.iloc[:,2]))
+# create data frames for yearly frequency of team, city and sport
+year_team_freq = year_team.unstack(level = -1).fillna(0)
+year_city_freq = year_city.unstack(level = -1).fillna(0)
+year_sport_freq = year_sport.unstack(level = -1).fillna(0)
+year_event_freq = year_event.unstack(level = -1).fillna(0)
+year_name_freq = year_name.unstack(level = -1).fillna(0)
+#year_team_freq.sort_values(by=years).head()
+
+#sort columns to get most frequent words in one year
+#year_team_freq = year_city_freq.apply(lambda x: x.sort_values(ascending=False).values)
+
+
+team_year_freq = year_team.unstack(level = 0).fillna(0)
+city_year_freq = year_city.unstack(level = 0).fillna(0)
+sport_year_freq = year_sport.unstack(level=0).fillna(0)
+event_year_freq = year_event.unstack(level = 0).fillna(0)
+
+# plot data
+#year_team_freq.plot.bar()
+#plt.show()
+
+top_team_freq = year_team_freq.filter(df_freq["team"])
+top_sport_freq = year_sport_freq.filter(df_freq["sport"])
+top_event_freq = year_sport_freq.filter(df_freq["event"])
+top_name_freq = year_name_freq.filter(df_freq["name"])
+
+
+
+
+
+# write to csv files
+# =============================================================================
+# year_team_freq.to_csv("year_team_freq.csv")
+# year_city_freq.to_csv("year_city_freq.csv")
+# year_sport_freq.to_csv("year_sport_freq.csv")
+# year_event_freq.to_csv("year_event_freq.csv")
+# 
+# team_year_freq.to_csv("team_year_freq.csv")
+# sport_year_freq.to_csv("sport_year_freq.csv")
+# event_year_freq.to_csv("_sport_freq.csv")
+# =============================================================================
+
+top_team_freq.to_csv("top_team_freq.csv")
+top_sport_freq.to_csv("top_sport_freq.csv")
+top_event_freq.to_csv("top_event_freq.csv")
+
+
+
+
+
+
+
+
