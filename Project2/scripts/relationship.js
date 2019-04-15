aa2d5792e927643583a0444403eaf54ff010aad4
gj0706 <32847054+gj0706@users.noreply.github.com>
2019-04-14 23:44:18 -0500

    changed node and link size

--- Project2/scripts/relationship.js
+++ Project2/scripts/relationship.js
@@ -28,86 +28,90 @@ var svg2 = d3.select("#graph").append("svg")
 
 var data1;
 
-d3.csv("data/test.csv",function(error,data){
+d3.csv("data/test.csv",function(error,data) {
     data1 = data;
     //set up an empty graph
-    graph = {"nodes" : [], "links" : []};
+    graph = {"nodes": [], "links": []};
 
     //get the headers for the data
     var keys = Object.keys(data[0]);
 
+    // create unique nodes and links: want nodes to be team(countries) and link targets to be year, sport and medal
+    // so that we can have a network to see which team(country) in which year got what type of medal in which sport.
 
-    // graph = {"nodes" : [], "links" : []};
-    // data.forEach(function(d){
-    //     adduniquenodes(d.name);
-    //     adduniquenodes(d.team);
-    //     adduniquenodes(d.sport);
-    //     adduniquenodes(d.event);
-    //     adduniquenodes(d.city);
-    //     adduniquenodes(d.year);
-        // graph.links.push ( {"source" : d.name,
-        //     "target" :d.team,
-        //     "value" : countvalues('name',d.name,'team',d.team)
+    data.forEach(function (d) {
+        adduniquenodes(d.year);
+        adduniquenodes(d.team);
+        adduniquenodes(d.sport);
+        adduniquenodes(d.medal);
+        // adduniquenodes(d.event);
+        // adduniquenodes(d.city);
+        // adduniquenodes(d.year);
+        graph.links.push({
+            "source": d.sport,
+            "target": d.medal,
+            "value": countvalues('sport', d.sport, 'medal', d.medal)
+        });
+
+        graph.links.push({
+            "source": d.year,
+            "target": d.team,
+            "value": countvalues('year', d.year, 'team', d.team)
+        });
+
+            graph.links.push ( {"source" : d.team,
+                "target" :d.sport,
+                "value" : countvalues('team',d.team,'sport',d.sport)
+            });
+        //     graph.links.push ( {"source" : d.sport,
+        //         "target" :d.event,
+        //         "value" : countvalues('sport',d.sport,'event',d.event)
+        //     });
+        });
+
+
+        // process the data to create nodes and links
+
+        // data.forEach(function(d) {
+        //     keys.forEach(function (key, i) {
+        //         if (d[key] != "none") {
+        //             adduniquenodes(d[key]);
+        //         } //add node if not "none"
+        //         var c = 1; //checks next column
+        //         if (d[keys[i + c]] != undefined && d[key] !== "none") {
+        //             while (d[keys[i + c]] === "none") {
+        //                 c = c + 1;     //jump to next column if "none" found in the column next
+        //             }
+        //             graph.links.push({
+        //                 "source": d[key],
+        //                 "target": d[keys[i + c]],
+        //                 "value": countvalues(key, d[key], keys[i + c], d[keys[i + c]])
+        //             });
+        //         }
+        //     })
         // });
-        //
-        // graph.links.push ( {"source" : d.team,
-        //     "target" :d.name,
-        //     "value" : countvalues('team',d.team,'name',d.name)
-        // });
-        // graph.links.push ( {"source" : d.team,
-        //     "target" :d.sport,
-        //     "value" : countvalues('team',d.team,'sport',d.sport)
-        // });
-    //     graph.links.push ( {"source" : d.sport,
-    //         "target" :d.event,
-    //         "value" : countvalues('sport',d.sport,'event',d.event)
-    //     });
-    // });
-
-
-
-
-    // process the data to create nodes and links
-
-    data.forEach(function(d) {
-        keys.forEach(function (key, i) {
-            if (d[key] != "none") {
-                adduniquenodes(d[key]);
-            } //add node if not "none"
-            var c = 1; //checks next column
-            if (d[keys[i + c]] != undefined && d[key] !== "none") {
-                while (d[keys[i + c]] === "none") {
-                    c = c + 1;     //jump to next column if "none" found in the column next
-                }
-                graph.links.push({
-                    "source": d[key],
-                    "target": d[keys[i + c]],
-                    "value": countvalues(key, d[key], keys[i + c], d[keys[i + c]])
-                });
-            }
-        })
-    });
         debugger
+
         function adduniquenodes(value) {
-            if (graph.nodes.indexOf(value) === -1){
-            //     if (graph.nodes.value > 10){
-                    graph.nodes.push(value);
+            if (graph.nodes.indexOf(value) === -1) {
+                //     if (graph.nodes.value > 10){
+                graph.nodes.push(value);
             }
         }
 
-        function countvalues (sourcekey, source, targetkey, target) {
+        function countvalues(sourcekey, source, targetkey, target) {
             var c = 0;
-            data.forEach (function (d){
-                if (d[sourcekey] === source && d[targetkey]===target){
+            data.forEach(function (d) {
+                if (d[sourcekey] === source && d[targetkey] === target) {
                     c++;
                 }
-
             });
             return c;
         }
-    console.log(graph);
 
-    // loop through each link replacing the text with its index from node
+        console.log(graph);
+
+        // loop through each link replacing the text with its index from node
         graph.links.forEach(function (d, i) {
             graph.links[i].source = graph.nodes.indexOf(graph.links[i].source);
             graph.links[i].target = graph.nodes.indexOf(graph.links[i].target);
@@ -116,121 +120,129 @@ d3.csv("data/test.csv",function(error,data){
         //now loop through each nodes to make nodes an array of objects
         // rather than an array of strings
         graph.nodes.forEach(function (d, i) {
-            graph.nodes[i] = { "name": d };
+            graph.nodes[i] = {"name": d};
         });
         // graph.links.filter(d=>d.value === true);
 
-debugger
+        // debugger
 
 
+        linkScale.domain(d3.extent(graph, function (d) {
+            return d.value;
+        }));
 
-    linkScale.domain(d3.extent(graph, function (d) {
-        return d.value;
-    }));
+        var force = d3.layout.force()
+            .nodes(graph.nodes)
+            .links(graph.links)
+            .size([width1, height1])
+            .linkDistance(200)
+            .charge(-800)
+            .on("tick", tick)
+            .start();
 
-    var force = d3.layout.force()
-        .nodes(graph.nodes)
-        .links(graph.links)
-        .size([width1, height1])
-        .linkDistance(60)
-        .charge(-300)
-        .on("tick", tick)
-        .start();
+        // svg2.append("svg:defs").selectAll('marker')
+        //     .data(['end'])
+        //     .enter()
+        //     .append("svg:marker")
+        //     .attr('id', function (d) {
+        //         return d;
+        //     })
+        //     .attr('viewBox', '0 -5 10 10')
+        //     .attr('refX', 15)
+        //     .attr('refY', -1.5)
+        //     .attr('markerWidth', 6)
+        //     .attr('markerHeight', 6)
+        //     .attr('orient', 'auto')
+        //     .append('svg:path')
+        //     .attr("d", "M0,-5L10,0L0,5");
 
-    svg2.append("svg:defs").selectAll('marker')
-        .data(['end'])
-        .enter()
-        .append("svg:marker")
-        .attr('id', function (d) {
-            return d;
-        })
-        .attr('viewBox', '0 -5 10 10')
-        .attr('refX', 15)
-        .attr('refY', -1.5)
-        .attr('markerWidth', 6)
-        .attr('markerHeight', 6)
-        .attr('orient', 'auto')
-        .append('svg:path')
-        .attr("d", "M0,-5L10,0L0,5");
+        var link = svg2.append("svg:g").selectAll("path")
+            .data(force.links())
+            .enter()
+            .append("svg:path")
+            // .filter(d=>d.weight > 10)
+            .classed("link", true)
+            .attr("marker-end", "url(#end)");
 
-    var path = svg2.append("svg:g").selectAll("path")
-        .data(force.links())
-        .enter()
-        .append("svg:path")
-        // .filter(d=>d.weight > 10)
-        .classed("link", true)
-        .attr("marker-end", "url(#end)");
-
-    var node = svg2.selectAll(".node")
-        .data(force.nodes())
-        .enter()
-        .append("g")
-        .classed("node", true)
-        .on("click", click)
-        .on("dblclick", dblclick)
-        .call(force.drag());
+        var node = svg2.selectAll(".node")
+            .data(force.nodes())
+            .enter()
+            .append("g")
+            .classed("node", true)
+            .on("click", click)
+            .on("dblclick", dblclick)
+            .on("mouseover", function(d){
+                link.style('stroke-width', function(l) {
+                    if (d === l.source || d === l.target)
+                        return 4;
+                    else
+                        return 1;
+                });
+            })
+            .on("mouseout", function(){link.style("stroke-width", 1)})
+            .call(force.drag());
         // .on("dblclick", connectedNodes());
 
-    node.append("circle")
-        .attr("r", 5)
-        .style("fill", function (d) {
-            return color1(d.name)
-        });
+        node.append("circle")
+            .attr("r", 10)
+            .style("fill", function (d) {
+                return color1(d.name)
+            });
 
 
-    node.append("text")
-        .attr("x", 12)
-        .attr("dy", ".35em")
-        .text(function (d) {
-            return d.name;
-        });
-
-    function tick() {
-        path.attr("d", function (d) {
-            var dx = d.target.x - d.source.x,
-                dy = d.target.y - d.source.y,
-                dr = Math.sqrt(dx * dx + dy * dy);
-
-            return "M" +
-                d.source.x + "," + d.source.y +
-                "A" + dr + "," + dr + " 0 0,1 " +
-                d.target.x + "," + d.target.y;
-        }).style("opacity", function (d) {
-            return linkScale(d.value);
-        });
-
-        node.attr("transform", function (d) {
-            return "translate(" + d.x + "," + d.y + ")";
-        });
-    }
-
-    function click() {
-        d3.select(this).select("text").transition()
-            .duration(750)
-            .attr("x", 22)
-            .style("fill", "steelblue")
-            .style("stroke", "lightsteelblue")
-            .style("stroke-width", ".5px")
-            .style("font", "20px sans-serif");
-
-        d3.select(this).select("circle").transition()
-            .duration(750)
-            .attr("r", 16);
-    }
-
-    function dblclick() {
-        d3.select(this).select("text").transition()
-            .duration(750)
+        node.append("text")
             .attr("x", 12)
-            .style("fill", "black")
-            .style("stroke", "none")
-            .style("stroke-width", "none")
-            .style("font", "10px sans-serif");
+            .attr("dy", ".35em")
+            .text(function (d) {
+                return d.name;
+            });
 
-        d3.select(this).select("circle").transition()
-            .duration(750)
-            .attr("r", 6);
-    }
+        function tick() {
+            link.attr("d", function (d) {
+                var dx = d.target.x - d.source.x,
+                    dy = d.target.y - d.source.y,
+                    dr = Math.sqrt(dx * dx + dy * dy);
+
+                return "M" +
+                    d.source.x + "," + d.source.y +
+                    "A" + dr + "," + dr + " 0 0,1 " +
+                    d.target.x + "," + d.target.y;
+            }).style("opacity", function (d) {
+                return linkScale(d.value);
+            });
+
+            node.attr("transform", function (d) {
+                return "translate(" + d.x + "," + d.y + ")";
+            });
+        }
+
+        function click() {
+            d3.select(this).select("text").transition()
+                .duration(750)
+                .attr("x", 22)
+                .style("fill", "steelblue")
+                .style("stroke", "lightsteelblue")
+                .style("stroke-width", ".5px")
+                .style("font", "20px sans-serif");
+
+            d3.select(this).select("circle").transition()
+                .duration(750)
+                .attr("r", 16);
+        }
+
+        function dblclick() {
+            d3.select(this).select("text").transition()
+                .duration(750)
+                .attr("x", 12)
+                .style("fill", "black")
+                .style("stroke", "none")
+                .style("stroke-width", "none")
+                .style("font", "10px sans-serif");
+
+            d3.select(this).select("circle").transition()
+                .duration(750)
+                .attr("r", 6);
+        }
 
 
 // //Toggle stores whether the highlighting is on
@@ -268,11 +280,11 @@ debugger
 //     }
 
 
-
-
 // debugger
 
 });
+
+
 
 
 function updataGraph(data){
